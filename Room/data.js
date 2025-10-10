c2bc68980e6e16c0cf080c180014e61872d273e6
Eagleking112 <55479149+Eagleking112@users.noreply.github.com>
2025-10-10 16:19:54 +0530

    Added 3d room

---
+++ Room/data.js
@@ -0,0 +1,235 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dwg1-1",
+      "name": "dwg1 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7332992297455103,
+          "pitch": 0.09873875030600487,
+          "rotation": 0,
+          "target": "1-kitchen_001"
+        },
+        {
+          "yaw": -1.440070193933252,
+          "pitch": 0.048451141321738334,
+          "rotation": 0,
+          "target": "2-room_1"
+        },
+        {
+          "yaw": -2.2503728066522797,
+          "pitch": 0.0722946496616359,
+          "rotation": 0,
+          "target": "3-room_2"
+        },
+        {
+          "yaw": 0.5696624277261932,
+          "pitch": 0.07697724909390224,
+          "rotation": 0,
+          "target": "4-room_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kitchen_001",
+      "name": "kitchen_001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 900,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1230281827562134,
+          "pitch": 0.1972097621970157,
+          "rotation": 0,
+          "target": "0-dwg1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-room_1",
+      "name": "room_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 270,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9109465479017995,
+          "pitch": 0.03231694889393388,
+          "rotation": 0,
+          "target": "0-dwg1-1"
+        },
+        {
+          "yaw": 2.7115564692093823,
+          "pitch": -0.03637630764665545,
+          "rotation": 0,
+          "target": "5-bath_01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-room_2",
+      "name": "Room_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7712210674107016,
+          "pitch": 0.05782500083945408,
+          "rotation": 0,
+          "target": "0-dwg1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-room_3",
+      "name": "room_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9462646794113407,
+          "pitch": -0.010301868415986348,
+          "rotation": 0,
+          "target": "0-dwg1-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bath_01",
+      "name": "bath_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 650,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3534657933782963,
+          "pitch": 0.11412526615962726,
+          "rotation": 0,
+          "target": "2-room_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
